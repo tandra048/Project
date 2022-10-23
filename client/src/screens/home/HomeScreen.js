e6d1663ca3fb83cc79c4217e4d2840ca80aba522
tandra048 <2048tandraju@gmail.com>
2022-10-23 10:47:03 +0600

    commit

---
+++ client/src/screens/home/HomeScreen.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import AuthRequired from "screens/auth/AuthRequired";
+import ChatBody from "screens/chatbody/ChatBody";
+import Sidebar from "screens/sidebar/sidebar";
+const HomeScreen = (props) => {
+  const [currentChattingMember, setCurrentChattingMember] = useState({});
+  const [onlineUserList, setOnlineUserList] = useState([]);
+  
+  return (
+    <main className="content">
+      <div className="container-fluid p-0">
+        <div className="container-fluid">
+          <div className="row g-0">
+            <Sidebar
+              setCurrentChattingMember={setCurrentChattingMember}
+              onlineUserList={onlineUserList}
+              {...props}
+            />
+            <ChatBody
+              setOnlineUserList={setOnlineUserList}
+              currentChattingMember={currentChattingMember}
+              {...props}
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AuthRequired(HomeScreen);
